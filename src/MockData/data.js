38db9f53fa6b8a58722976a71d2c36425f14c724
ratulislam46 <arfanratul46@gmail.com>
2025-08-15 23:53:20 +0600

    responsive navbar testing

---
+++ src/MockData/data.js
@@ -0,0 +1,22 @@
+export const NavbarMenu =[
+    {
+        id:1,
+        title:'home',
+        link:"/",
+    },
+    {
+        id:2,
+        title:'About',
+        link:"#",
+    },
+    {
+        id:3,
+        title:'Program',
+        link:"#",
+    },
+    {
+        id:4,
+        title:'Blogs',
+        link:"#",
+    },
+]
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { NavbarMenu } from '../../MockData/data';
+import { FaDumbbell } from 'react-icons/fa';
+import { MdMenu } from 'react-icons/md';
+import { CiSearch } from 'react-icons/ci';
+import { PiShoppingCartThin } from 'react-icons/pi';
+
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            <nav>
+                <div className='container mx-auto flex items-center justify-between py-8'>
+                    {/* logo section  */}
+                    <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
+                        <FaDumbbell />
+                        <p>Coders</p>
+                        <p className='text-orange-400'>Gyms</p>
+                    </div>
+                    {/* Menu section  */}
+                    <div className='hidden md:block'>
+                        <ul className='flex items-center gap-2 font-bold uppercase'>
+                            {
+                                NavbarMenu.map((item) => {
+                                    return (
+                                        <li key={item.id}>
+                                            <a href={item.link} className='inline-blocklock py-1 px-3 hover:text-orange-400 font-semibold'> {item.title} </a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    {/* lcons section  */}
+                    <div className='flex items-center gap-4'>
+                        <button className='text-2xl hover:bg-orange-500 hover:text-white rounded-full p-2'>
+                            <CiSearch></CiSearch>
+                        </button>
+                        <button className='text-2xl hover:bg-orange-500 hover:text-white rounded-full p-2'>
+                            <PiShoppingCartThin />
+                        </button>
+                        <button className='font-semibold rounded-md border-2 px-6 py-2 text-orange-500 border-orange-500 hover:text-white hover:bg-orange-500 hidden md:block'>
+                            Login
+                        </button>
+                    </div>
+                    {/* Mobile hamburger menu section  */}
+                    <div className='md:hidden' onClick={() => setOpen(!open)}>
+                        <MdMenu className='text-4xl' />
+                    </div>
+                </div>
+            </nav>
+
+            {/* Mobile Sidebar section  */}
+        </>
+    );
+};
+
+export default Navbar;
